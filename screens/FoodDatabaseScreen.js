6570d94284c8f5ec94c56a0f542efa66164db9ba
Maryline Fernandes <maryline.fernandes@edu.esiee.fr>
2023-05-30 11:00:52 +0200

    search bar + useState

--- screens/FoodDatabaseScreen.js
+++ screens/FoodDatabaseScreen.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Text, TextInput } from '@react-native-material/core';
+import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 
 const FoodDatabaseScreen = () => {
+  const [searchText, setSearchText] = useState();
+
   return (
     <View>
-      <Text>Welcome to the FoodDatabaseScreen!</Text>
+      <TextInput
+        variant="standard"
+        label="Search"
+        leading={(props) => <Icon name="magnify" {...props} />}
+        style={{ margin: 16 }}
+        onChangeText={setSearchText}
+      />
+      <Text>{searchText}</Text>
     </View>
   );
 };
