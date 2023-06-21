f81739b6521d13c0ced62aa69e732ac3a0c1ac4b
Maryline Fernandes <maryline.fernandes@edu.esiee.fr>
2023-06-21 20:55:58 +0200

    autocomplete ok

--- screens/FoodDatabaseScreen.js
+++ screens/FoodDatabaseScreen.js
@@ -3,15 +3,17 @@ import { SafeAreaView, View, FlatList } from 'react-native';
 import { ListItem, TextInput } from '@react-native-material/core';
 import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 import ApiService from '../js/ApiService';
-import { REACT_APP_API_AUTOCOMPLETE } from '@env';
+import { REACT_APP_API_AUTOCOMPLETE, REACT_APP_API_URL } from '@env';
 
 const FoodDatabaseScreen = () => {
   const [searchText, setSearchText] = useState('');
   const [suggestions, setSuggestions] = useState({});
 
-  const handleSearchSubmit = (event) => {
+  const handleSuggestionClick = async (event) => {
+    console.log('salut ');
     setSearchText(event.nativeEvent.text);
-    //ApiService.getData(REACT_APP_API_URL, '?ingr=apple');
+    const answer = await ApiService.getData(REACT_APP_API_URL, '?ingr=apple');
+    console.log(answer);
   };
 
   const handleChange = async (event) => {
@@ -36,16 +38,15 @@ const FoodDatabaseScreen = () => {
           placeholder="Search"
           leading={(props) => <Icon name="magnify" {...props} />}
           style={{ margin: 16 }}
-          onSubmitEditing={handleSearchSubmit}
           onChange={handleChange}
-          // onSubmitEditing={(event) => setSearchText(event.nativeEvent.text)}
+          onSubmitEditing={handleChange}
         />
       </View>
       <View>
         {suggestions && (
           <FlatList
             data={suggestions}
-            renderItem={({ item }) => <ListItem title={item} />}
+            renderItem={({ item }) => <ListItem onPress={handleSuggestionClick} title={item} />}
             keyExtractor={(item) => item}
           />
         )}
